f80ee7a0241f999c3b42d66975710c5a45f5aaf3
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-06-14 13:09:42 +0200

    Created empty jobBox component

---
+++ src/app/components/job-box/job-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-job-box',
+  templateUrl: './job-box.component.html',
+  styleUrls: ['./job-box.component.css']
+})
+export class JobBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
