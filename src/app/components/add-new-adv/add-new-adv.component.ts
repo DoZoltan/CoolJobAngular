0b5eba833ac08d67411edf7fddd10d2c79fe1b31
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-06-14 13:08:59 +0200

    Created empty addNewAdv component

---
+++ src/app/components/add-new-adv/add-new-adv.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-adv',
+  templateUrl: './add-new-adv.component.html',
+  styleUrls: ['./add-new-adv.component.css']
+})
+export class AddNewAdvComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
